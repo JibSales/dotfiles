1b27223976b6470f1ef4e5569d6f5a8050976d15
Sean Quinn <srquinn21@gmail.com>
2016-12-30 22:36:11 -0800

    Rename zsh plugins to modules

--- zsh/loader.zsh
+++ zsh/loader.zsh
@@ -2,20 +2,16 @@ echo "Creating a zgen save"
 
 # prezto options
 zgen prezto prompt theme 'sorin'
+zgen prezto module editor key-bindings 'vi'
+
+# make things pretty
+zgen load zsh-users/zsh-syntax-highlighting
+zgen load zsh-users/zsh-history-substring-search
 
 # prezto and modules
 zgen prezto
 zgen prezto git
 zgen prezto command-not-found
-zgen prezto syntax-highlighting
-
-# custom plugins
-zgen load "${ZDOTDIR:-$HOME}/.dotfiles/zsh/plugins/colors.zsh"
-zgen load "${ZDOTDIR:-$HOME}/.dotfiles/zsh/plugins/docker.zsh"
-zgen load "${ZDOTDIR:-$HOME}/.dotfiles/zsh/plugins/helpers.zsh"
-zgen load "${ZDOTDIR:-$HOME}/.dotfiles/zsh/plugins/key-bindings.zsh"
-zgen load "${ZDOTDIR:-$HOME}/.dotfiles/zsh/plugins/nvm.zsh"
-zgen load "${ZDOTDIR:-$HOME}/.dotfiles/zsh/plugins/phantomjs.zsh"
 
 # save all to the init script
 zgen save
