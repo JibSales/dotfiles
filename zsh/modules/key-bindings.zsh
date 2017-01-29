0cb72f10d04b49e5bb40140ce2d7fb78514280bb
Sean Quinn <srquinn21@gmail.com>
2017-01-29 14:34:12 -0800

    Adds support for no local dotfile directory

--- zsh/modules/key-bindings.zsh
+++ zsh/modules/key-bindings.zsh
@@ -1,9 +1,13 @@
-# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html
-# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Builtins
-# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Standard-Widgets
-
-# bindkey -v                                          # Use vi key bindings
-# bindkey '^r' history-incremental-search-backward    # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
-
 # Map 'jj' to ESC when in insert mode
 bindkey -M viins 'jj' vi-cmd-mode
+
+# [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
+bindkey '^r' history-incremental-search-backward
+
+# Search backwards and forwards with a pattern
+bindkey -M vicmd '/' history-incremental-pattern-search-backward
+bindkey -M vicmd '?' history-incremental-pattern-search-forward
+
+# set up for insert mode too
+# bindkey -M viins '^R' history-incremental-pattern-search-backward
+# bindkey -M viins '^F' history-incremental-pattern-search-forward
