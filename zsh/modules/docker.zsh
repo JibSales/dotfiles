5124922d04a35b93d178a1b16fef1d9cfec99a69
Sean Quinn <srquinn@amazon.com>
2017-04-24 09:02:43 -0700

    Adds rbenv support and jshint in vim

--- zsh/modules/docker.zsh
+++ zsh/modules/docker.zsh
@@ -7,6 +7,11 @@ alias dra='docker rm $(docker ps -aq)'
 alias dps='docker ps'
 alias dpS='docker ps -a'
 
+function remove_old_ground {
+  local num_to_keep=${1:-4}
+  docker rmi -f $(docker images | grep adnground-1 | sort -r | tail +$num_to_keep | awk '{print $3}')
+}
+
 function dsh {
   docker exec -it $1 /bin/bash
 }
--- zsh/modules/helpers.zsh
+++ zsh/modules/helpers.zsh
@@ -5,3 +5,8 @@ function set_ip {
   export EXTERNAL_IP=$({ ifconfig utun0 2>/dev/null || ifconfig en5 2>/dev/null || ifconfig en0 } | awk '$1 == "inet" {print $2}')
   echo -e "EXTERNAL_IP set to \033[0;36m$EXTERNAL_IP\033[0m"
 }
+
+function kill-all {
+  local pattern=$1
+  ps -ef | grep $1 | grep -v grep | awk '{print $3}' | xargs kill -9
+}
