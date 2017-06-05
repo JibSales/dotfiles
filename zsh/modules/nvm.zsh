a1a83e4ac5873aec118ea344fab886d546560918
Sean Quinn <srquinn@amazon.com>
2017-06-05 20:27:29 +0000

    Adds better support for NVM on cloud desktop

--- zsh/modules/nvm.zsh
+++ zsh/modules/nvm.zsh
@@ -1,4 +1,7 @@
 if [[ "$OSTYPE" == darwin* ]]; then
   export NVM_DIR=~/.nvm
   source $(brew --prefix nvm)/nvm.sh
+else
+  export NVM_DIR="/local${HOME}/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 fi
