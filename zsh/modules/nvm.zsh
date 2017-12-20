9f65689bb63b95fcd8874c1ed037c0afb4014314
Sean Quinn <srquinn@amazon.com>
2017-12-20 22:53:37 +0000

    Remove NVM from non osx setups

--- zsh/modules/nvm.zsh
+++ zsh/modules/nvm.zsh
@@ -1,7 +1,5 @@
 if [[ "$OSTYPE" == darwin* ]]; then
   export NVM_DIR=~/.nvm
   source $(brew --prefix nvm)/nvm.sh
-else
-  export NVM_DIR="/local${HOME}/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 fi
+
