1dd2916664a8ef56457ca7acf20006b3bb57a957
Sean Quinn <srquinn@amazon.com>
2019-09-03 18:25:19 +0000

    Properly colors the PS1 cloud for ssh sessions

--- zsh/modules/prompt.zsh
+++ zsh/modules/prompt.zsh
@@ -1,1 +1,1 @@
-PROMPT='${SSH_TTY:+"%F{8}☁ %f"} %F{4}${_prompt_sorin_pwd}%(!. %B%F{1}#%f%b.)${editor_info[keymap]} '
+PROMPT='${SSH_TTY:+"%F{7}☁ %f"} %F{4}${_prompt_sorin_pwd}%(!. %B%F{1}#%f%b.)${editor_info[keymap]} '
