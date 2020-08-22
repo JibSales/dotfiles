836d87fe9402b7a1a5c3db82f32784ced4feae5e
⛵️Sean Quinn <srquinn@amazon.com>
2020-08-22 18:04:24 +0000

    Adds better typescript support

--- zsh/modules/prompt.zsh
+++ zsh/modules/prompt.zsh
@@ -6,8 +6,13 @@ if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
   fi
 fi
 
+function update_base_directory() {
+  local base_directory=$(basename $(pwd))
+  echo "%B%F{7}${base_directory}%f%b"
+}
+
 zstyle ':prezto:module:editor:info:keymap:primary' format '%B%F{4}❯%f%b'
 zstyle ':prezto:module:editor:info:keymap:alternate' format '%B%F{5}❮%f%b'
 zstyle ':prezto:module:git:info:branch' format ' %%B%F{2}%b%f%%b'
 
-PROMPT='$CUSTOM_REMOTE_SESSION_PROMPT%F{4}%(!. %B%F{1}#%f%b.)${editor_info[keymap]} '
+PROMPT='$CUSTOM_REMOTE_SESSION_PROMPT${editor_info[keymap]} '
