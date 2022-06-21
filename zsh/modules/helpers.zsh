644a4efb33f90c3d6f0582030fcc591e4448deb3
⛵️ Sean Quinn <srquinn@amazon.com>
2022-06-21 13:47:19 -0400

    Adds ssh copy function

--- zsh/modules/helpers.zsh
+++ zsh/modules/helpers.zsh
@@ -1,6 +1,10 @@
 # Clear the formatting from the text in the clipboard
 alias clf="pbpaste | pbcopy"
 
+function spbcopy {
+  ssh devbox "cat $1" | pbcopy
+}
+
 function set_ip {
   export EXTERNAL_IP=$({ ifconfig utun0 2>/dev/null || ifconfig en5 2>/dev/null || ifconfig en0 } | awk '$1 == "inet" {print $2}')
   echo -e "EXTERNAL_IP set to \033[0;36m$EXTERNAL_IP\033[0m"
